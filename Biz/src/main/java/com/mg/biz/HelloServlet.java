32525bb9c2bb25f952c17af2a460ecedf8ecb988
cmg <cmg@121.173.242.159>
2019-02-14 22:57:41 +0900

    Study about Servlet

---
+++ Biz/src/main/java/com/mg/biz/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.mg.biz;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//이 페이지는 Servlet이 어떻게 동작하는지 확인하기 위한 페이지 입니다.
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public HelloServlet() {
+		System.out.println("===> HelloServlet 객체 생성");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("doGet() 메소드 호출");
+	}
+
+}
